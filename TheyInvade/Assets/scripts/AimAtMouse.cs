874baa0c282123ee97e1ce18608ed3ae29edf60a
BobbyAKing <48815552+BobbyAKing@users.noreply.github.com>
2019-04-08 10:34:34 +1000

    added some bits to play around with
    basic player sptire
    basic tile sprite
    some scripting bsed off unity tutorials for physics objects and player controller
    had a go at making an aiming script so the the head and arms of player are pointing at mouse and flips sprite when mouse is on either side of player.
    sorry the scripts arent very descriptive, i was just following the tutorials...

---
+++ TheyInvade/Assets/scripts/AimAtMouse.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimAtMouse : MonoBehaviour
+{
+
+    bool facingRight = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+
+        if (mousePos.x >= 0 && !facingRight)
+        {
+            facingRight = true;
+        }
+        else if (mousePos.x < 0 && facingRight)
+        {
+            facingRight = false;
+        }
+
+        Vector3 mousePosition = Input.mousePosition;
+        Vector3 lookPos = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        lookPos = lookPos - transform.position;
+
+        if (!facingRight)
+        {
+            lookPos.y *= -1;
+            lookPos.x *= -1;
+        }
+
+        float aimAngle = Mathf.Atan2(lookPos.y, lookPos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(aimAngle, Vector3.forward);
+
+
+
+    }
+}
