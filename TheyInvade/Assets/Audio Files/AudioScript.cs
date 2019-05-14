1a17c102e274819c37dc4d4919c2220b107a6219
liam-hallinan <48878901+liam-hallinan@users.noreply.github.com>
2019-05-14 23:33:11 +1000

    Player Gun audio working and enemy file uploaded
    Gun now makes sound when fires
    
    Just need the finalised alien script to figure out how to trigger alein audio file
    
    Music will be up later

---
+++ TheyInvade/Assets/Audio Files/AudioScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+
+    public AudioClip playerGun;
+    public AudioClip enemyGun;
+
+    public AudioSource playerGunSource;
+    public AudioSource enemyGunSource; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerGunSource.clip = playerGun;
+        enemyGunSource.clip = enemyGun;
+
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (Input.GetButtonDown("Fire1"))
+        {
+            playerGunSource.Play();
+        }
+
+    }
+}
